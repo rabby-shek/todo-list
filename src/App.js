92769ce84e077572729a7d05c12f625b086f01f6
rabby-shek <rabbyshek1998@gmail.com>
2024-07-02 15:37:35 +0600

    app component code formatted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/navbar/Navbar';
 import Main from './components/main/Main';
 import './App.css';
